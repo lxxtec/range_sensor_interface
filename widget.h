a27fef1615322f654f11930e2b990f4f1bff06ac
lxxtec <631859877@qq.com>
2022-06-08 11:48:52 +0800

    update

--- widget.h
+++ widget.h
@@ -1,52 +1,53 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-#include <QWidget>
-#include "surfacegraph.h"
 #include "serialport.h"
+#include "surfacegraph.h"
+#include <QDebug>
+#include <QList>
+#include <QTimer>
+#include <QWidget>
+#include <QtDataVisualization>
+#include <QtGui/QPainter>
+#include <QtGui/QScreen>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QtSerialPort/QtSerialPort>
 #include <QtWidgets/QApplication>
-#include <QtWidgets/QWidget>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QMessageBox>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QRadioButton>
 #include <QtWidgets/QSlider>
-#include <QtWidgets/QGroupBox>
-#include <QtWidgets/QComboBox>
-#include <QtWidgets/QLabel>
-#include <QtWidgets/QMessageBox>
-#include <QtGui/QPainter>
-#include <QtGui/QScreen>
-#include <QtDataVisualization>
-#include <QDebug>
-#include <QtSerialPort/QtSerialPort>
-#include <QList>
-#include <QtSerialPort/QSerialPortInfo>
-#include <QTimer>
-#include <vector>
+#include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
 #include <math.h>
+#include <vector>
 #define _USE_MATH_DEFINES
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class Widget; }
+namespace Ui {
+class Widget;
+}
 QT_END_NAMESPACE
 
-class Widget : public QWidget
-{
+class Widget : public QWidget {
     Q_OBJECT
 
-public:
+  public:
     Widget(QWidget *parent = nullptr);
     ~Widget();
 
-public:
+  public:
     void on_serial_recvd();
 
     std::vector<float> process_data(QByteArray &bytes);
 
-    QVector3D to3DPoint(float angleH,float angleV,float distance);
+    QVector3D to3DPoint(float angleH, float angleV, float distance);
 
-private slots:
+  private slots:
     void on_btClearR_clicked();
 
     void on_btClearS_clicked();
@@ -55,20 +56,17 @@ private slots:
 
     void on_btSaveR_clicked();
 
-    void on_pushButton_6_clicked();
-
-    void on_pushButton_4_clicked();
-
     void on_btStartScan_clicked();
 
-private:
+  private:
     Ui::Widget *ui;
     Q3DSurface *graph;
     QWidget *container;
     SurfaceGraph *modifier;
     QTimer *timer;
-private:
-    SerialPort  *port;
+
+  private:
+    SerialPort *port;
     QList<QSerialPortInfo> portlist;
 };
 #endif // WIDGET_H
